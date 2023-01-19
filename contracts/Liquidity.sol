17a5cd8051011c0f0264df1ee8636abd6fc7cbf2
Henrychang26 <henrychang26@gmail.com>
2023-01-19 13:27:57 -0800

    Uniswap Clone

--- contracts/Liquidity.sol
+++ contracts/Liquidity.sol
@@ -10,6 +10,8 @@ import "@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.s
 import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
 import "@uniswap/v3-periphery/contracts/base/LiquidityManagement.sol";
 
+error Liquidity__NotOwner();
+
 contract Liquidity is IERC721Receiver, LiquidityManagement {
   struct Deposit {
     address owner;
@@ -156,5 +158,96 @@ contract Liquidity is IERC721Receiver, LiquidityManagement {
 
     TransferHelper.safeTransfer(token0, owner, amount0);
     TransferHelper.safeTransfer(token1, owner, amount1);
+  }
+
+  function decreaseLiquidity(
+    uint128 liquidity
+  ) external returns (amount0, amount1) {
+    if (msg.sender != deposits[tokenId].owner) {
+      revert Liquidity__NotOwner();
+    }
+    INonfungiblePositionManager.decreaseLiquidityParams
+      memory params = INonfungiblePositionManager.decreaseLiquidityParams({
+        tokenId: tokenId,
+        liquidit: liquidity,
+        amount0Min: 0,
+        amount1Min: 0,
+        deadline: block.timestamp + 15
+      });
+
+    (amount0, amount1) = nonfungiblePositionManager.decreaseLiquidity(params);
+
+    _sendToOwner(tokenId, amount0, amount1);
+  }
+
+  function _sendToOwner(
+    uint256 tokenId,
+    uint256 amount0,
+    uint256 amount1
+  ) internal {
+    address owner = deposits[tokenId].owner;
+
+    address token0 = deposits[tokenId].token0;
+    address token1 = deposits[tokenId].token1;
+    TransferHelper.safeTransfer(token0, owner, amount0);
+    TransferHelper.safeTransfer(token1, owner, amount1);
+  }
+
+  function increaseLiquidity(
+    uint256 tokenId,
+    uint256 amountAdd0,
+    uint256 amountAdd1
+  ) external returns (uint128 liquidity, uint256 amount0, uint256 amount1) {
+    TransferHelper.safeTransfer(
+      deposits[tokenId].token0,
+      msg.sender,
+      address(this),
+      amountAdd0
+    );
+    TransferHelper.safeTransfer(
+      deposits[tokenId].token1,
+      msg.sender,
+      address(this),
+      amountAdd1
+    );
+
+    TransferHelper.safeApprove(
+      deposits[tokenId].token0,
+      address(nonfungiblePositionManager),
+      amountAdd0
+    );
+
+    TransferHelper.safeApprove(
+      deposits[tokenId].token1,
+      address(nonfungiblePositionManager),
+      amountAdd1
+    );
+
+    INonfungiblePositionManager.increaseLiquidityParams
+      memory params = INonfungiblePositionManager.increaseLiquidityParams({
+        tokenId: tokenId,
+        amount0Desired: amountAdd0,
+        amount1Desired: amountAdd1,
+        amount0Min: 0,
+        amount1Min: 0,
+        deadline: block.timestamp + 15
+      });
+
+    (liquidity, amount0, amount1) = nonfungiblePositionManager
+      .increaseLiquidity(params);
+  }
+
+  function retrieveNFT(uint256 tokenId) external {
+    if (msg.sender != deposits[tokenId].owner) {
+      revert Liquidity__NotOwner();
+    }
+
+    nonfungiblePositionManager.safeTransferFrom(
+      address(this),
+      msg.sender,
+      tokenId
+    );
+
+    delete deposits[tokenId];
   }
 }
